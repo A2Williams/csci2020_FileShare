b563ea33cac9169c58879b04ad6d257fb2e713e9
A2Williams <aaronchristoph.williams@uoit.net>
2018-03-26 08:48:12 -0400

    Added client, server and handler files

--- src/sample/FileClient.java
+++ src/sample/FileClient.java
@@ -6,18 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class FileClient extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("File Sharer");
+        primaryStage.setScene(new Scene(root, 600, 800));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
+
         launch(args);
     }
 }
