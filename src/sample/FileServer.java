30a0ed81408118905cb49730902c7ac3a0a98341
A2Williams <aaronchristoph.williams@uoit.net>
2018-03-27 13:12:03 -0400

    final Server class file.

--- src/sample/FileServer.java
+++ src/sample/FileServer.java
@@ -1,4 +1,38 @@
 package sample;
 
+import java.io.*;
+import java.net.*;
+
 public class FileServer {
+    private ServerSocket serverSocket = null;
+
+    public FileServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    public void fileRequests() throws IOException {
+        System.out.println("Server initialized on port!");
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            ClientConnectionHandler handler =
+                    new ClientConnectionHandler(clientSocket);
+            handler.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 8080;
+
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        try {
+            FileServer server = new FileServer(port);
+            server.fileRequests();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
