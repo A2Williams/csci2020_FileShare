8274379edc634aa4eceba32cc884898bf5f9ba2d
A2Williams <aaronchristoph.williams@uoit.net>
2018-03-28 08:57:36 -0400

    removed passing ports added directory file

--- src/sample/FileServer.java
+++ src/sample/FileServer.java
@@ -5,34 +5,43 @@ import java.net.*;
 
 public class FileServer {
     private ServerSocket serverSocket = null;
+    private File dir = null;
 
-    public FileServer(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
+    public FileServer(int port, String dir) throws IOException {
+        // initializes server socket connection and sharing directory
+        // throws @ socket initialization
+        if (new File(dir).isDirectory()) {
+            this.dir = new File(dir);
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server initialized on port!");
+        } else {
+            System.err.println("ERROR: shared folder has not been given");
+        }
     }
 
     public void fileRequests() throws IOException {
-        System.out.println("Server initialized on port!");
-
+        // accepts client connections
+        // throws @ accepting socket
         while (true) {
             Socket clientSocket = serverSocket.accept();
             ClientConnectionHandler handler =
-                    new ClientConnectionHandler(clientSocket);
+                    new ClientConnectionHandler(
+                            clientSocket, dir.getPath());
             handler.start();
         }
     }
 
     public static void main(String[] args) {
-        int port = 8080;
-
-        if (args.length > 0) {
-            port = Integer.parseInt(args[0]);
-        }
+        int port = 49000;
 
         try {
-            FileServer server = new FileServer(port);
+            FileServer server = new FileServer(port, args[0]);
             server.fileRequests();
         } catch (IOException e) {
+            System.err.println("ERROR: " +
+                    "IOException while creating server connection");
             e.printStackTrace();
+            System.exit(-1);
         }
     }
 }
