19ae56bc6844fd68ffe23811abd7f8b6252505b7
A2Williams <aaronchristoph.williams@uoit.net>
2018-03-28 08:54:23 -0400

    Added handling parameters

--- src/sample/FileClient.java
+++ src/sample/FileClient.java
@@ -6,18 +6,36 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.List;
+
 public class FileClient extends Application {
+    protected List<String> params = this.getParameters().getRaw();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        try {
+            String host = params.get(0);
+            String path = params.get(1);
+            controller.setParams(host, path);
+        } catch (Exception e) {
+            System.err.println("ERROR: parameters not correct");
+        }
         primaryStage.setTitle("File Sharer");
         primaryStage.setScene(new Scene(root, 600, 800));
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-
-        launch(args);
+        if (2 == args.length) {
+            launch(args);
+        } else {
+            System.err.println("ERROR: few arguments usage: " +
+                    "javac FileClient.java <hostname> <shared folder>");
+            System.exit(-1);
+        }
     }
 }
